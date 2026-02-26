1bdc278eb2314fb36ec2e33c279a8376124af679
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2026-02-25 20:15:56 -0500

    Small update to main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if not all([SUPABASE_URL, SUPABASE_KEY, TELEGRAM_TOKEN]):
 
 
 def main():
-    print("Starting A-to-Z Dispatch MVP... Ready for dispatch! 🚀")
+    print("Starting A-to-Z Dispatch MVP v1.0... Ready for dispatch! 🚀")
     
     # Initialize Supabase Client
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
